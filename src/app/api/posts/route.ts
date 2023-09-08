ca5d500140dae7136dd255ee52c10ab00d279f3e
natellu <t.le0nh4rdt@gmail.com>
2023-09-08 18:16:27 +0200

    table init, search

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -12,10 +12,42 @@ export async function GET(req: Request) {
     )
         return new Response("Unauthorized", { status: 401 })
 
+    const url = new URL(req.url)
     try {
+        const { page, limit, search } = z
+            .object({
+                page: z.string(),
+                limit: z.string(),
+                search: z.string().optional()
+            })
+            .parse({
+                page: url.searchParams.get("page"),
+                limit: url.searchParams.get("limit"),
+                search: url.searchParams.get("search") || undefined
+            })
+
+        //transaction?
+
+        const countPosts = await db.post.count({
+            where: {
+                title: {
+                    contains: search || undefined,
+                    mode: "insensitive"
+                }
+            }
+        })
+
         const posts = await db.post.findMany({
+            take: parseInt(limit),
+            skip: (parseInt(page) - 1) * parseInt(limit),
             orderBy: {
                 title: "asc"
+            },
+            where: {
+                title: {
+                    contains: search || undefined,
+                    mode: "insensitive"
+                }
             },
             select: {
                 author: true,
@@ -32,7 +64,12 @@ export async function GET(req: Request) {
             }
         })
 
-        return new Response(JSON.stringify(posts))
+        return new Response(
+            JSON.stringify({
+                posts,
+                count: countPosts
+            })
+        )
     } catch (error) {
         if (error instanceof z.ZodError)
             return new Response(error.message, { status: 422 })
